4084cf1b029493584f0c1bedd3b1097541e7d3c6
Dmitry Lobanov <lobanovds@gmail.com>
2025-06-20 04:17:23 +0500

    feat: Fin

--- 01/src/main.tf
+++ 01/src/main.tf
@@ -2,36 +2,53 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.6.2"
+      version = "~> 3.0.1"
     }
   }
   required_version = ">=1.8.4"
 }
 
-provider "docker" {}
-
-resource "docker_buildx_builder" "remote_docker_builder" {
-  name = "hw_context"
-  use = true
-  connection {
+provider "docker" {
     host = "ssh://debian@51.250.47.241"
-  }
-  remote {
-    key = "C:\\Users\\loban\\.ssh\\id_rsa"
-  }
+    ssh_opts = ["-i", "./id_rsa"]
 }
 
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = true
+
+resource "docker_image" "mysql" {
+  name         = "mysql:8"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "hello_world"
+resource "random_password" "random_mysql_root_password" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+resource "random_password" "random_mysql_password" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+
+resource "docker_container" "mysql_driver" {
+  image = docker_image.mysql.image_id
+  name  = "mysql"
+  restart = "always"
+  env = [
+    "MYSQL_ROOT_PASSWORD=${random_password.random_mysql_root_password.result}",
+    "MYSQL_DATABASE=wordpress",
+    "MYSQL_USER=wordpress",
+    "MYSQL_PASSWORD=${random_password.random_mysql_password.result}",
+    "MYSQL_ROOT_HOST=\"%\""
+  ]
 
   ports {
-    internal = 80
-    external = 9090
+    internal = 3306
+    external = 3306
   }
 }
